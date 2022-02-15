333bbeb30ee1dbb8eab5c766112f99662a3483cc
Ihimekpen Osemudiamen Andrew <oihimekpen@gmail.com>
2022-02-15 11:30:35 +0100

    database deployment issue 1

--- connection.js
+++ connection.js
@@ -4,12 +4,12 @@
 const {Client} = require('pg')
 
 const client = new Client({
-    host: "host.docker.internal",
+    host: "ec2-54-83-21-198.compute-1.amazonaws.com"
     // host: "localhost",
-    user: "postgres",
+    user: "xnwumytihbkzgc",
     port: 5432,
-    password: "rootUser",
-    database: "postgres"
+    password: "deaacc7f0aaf9f5bd6c0a420791b326a83cb228fffee617ce81f25bd8abf368d",
+    database: "dalokhaursqjcq"
 })
 
 module.exports = client
