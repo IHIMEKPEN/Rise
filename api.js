4bec1e22b5e11a1b6f36f98e33634b2a476f087c
Ihimekpen Osemudiamen Andrew <oihimekpen@gmail.com>
2022-02-15 15:52:13 +0100

    Update api.js

--- api.js
+++ api.js
@@ -210,41 +210,41 @@ app.get("/video", function (req, res) {
   videoStream.pipe(res);
 });
 
-// stream audio api
+// // stream audio api
 
-app.get("/audio", function (req, res) {
-  // Ensure there is a range given for the video
-  const range = req.headers.range;
-  if (!range) {
-    res.status(400).send("Requires Range header");
-  }
+// app.get("/audio", function (req, res) {
+//   // Ensure there is a range given for the video
+//   const range = req.headers.range;
+//   if (!range) {
+//     res.status(400).send("Requires Range header");
+//   }
 
-  // get video stats (about 61MB)
-  const videoPath = "Cold.mp3";
-  const videoSize = fs.statSync("Cold.mp3").size;
+//   // get video stats (about 61MB)
+//   const videoPath = "Cold.mp3";
+//   const videoSize = fs.statSync("Cold.mp3").size;
 
-  // Parse Range
-  // Example: "bytes=32324-"
-  const CHUNK_SIZE = 10 ** 1; 
-  const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+//   // Parse Range
+//   // Example: "bytes=32324-"
+//   const CHUNK_SIZE = 10 ** 1; 
+//   const start = Number(range.replace(/\D/g, ""));
+//   const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
-  // Create headers
-  const contentLength = end - start + 1;
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mp4",
-  };
+//   // Create headers
+//   const contentLength = end - start + 1;
+//   const headers = {
+//     "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+//     "Accept-Ranges": "bytes",
+//     "Content-Length": contentLength,
+//     "Content-Type": "video/mp4",
+//   };
 
-  // HTTP Status 206 for Partial Content
-  res.writeHead(206, headers);
+//   // HTTP Status 206 for Partial Content
+//   res.writeHead(206, headers);
 
-  // create video read stream for this particular chunk
-  const videoStream = fs.createReadStream(videoPath, { start, end });
+//   // create video read stream for this particular chunk
+//   const videoStream = fs.createReadStream(videoPath, { start, end });
 
-  // Stream the video chunk to the client
-  videoStream.pipe(res);
-});
+//   // Stream the video chunk to the client
+//   videoStream.pipe(res);
+// });
 
