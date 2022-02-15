06b27a50800a715bae45c77f5bc124a137f4dad0
Ihimekpen Osemudiamen Andrew <oihimekpen@gmail.com>
2022-02-15 13:01:11 +0100

    ssl fixed

--- connection.js
+++ connection.js
@@ -1,11 +1,14 @@
 // import { createRequire } from 'module';
 // const require = createRequire(import.meta.url);
+require('dotenv').config();
 
 const {Client} = require('pg')
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true 
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // const client = new Client({
